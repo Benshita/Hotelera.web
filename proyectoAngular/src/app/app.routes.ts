54e5a7f6ad58aa5135867f2be50fc038287a4e82
ByshoBB <vicente.alcaino@inacapmail.cl>
2025-07-17 00:02:28 -0400

    cambios

--- proyectoAngular/src/app/app.routes.ts
+++ proyectoAngular/src/app/app.routes.ts
@@ -4,11 +4,14 @@ import { HotelPagos } from './pages/hotel-pagos/hotel-pagos';
 import { HotelReservas } from './pages/hotel-reservass/hotel-reservas';
 import { HotelUsuariosComponent } from './pages/hotel-usuarios/hotel-usuarios';
 import { PaginaContactameComponent } from './pages/pagina-contactame/pagina-contactame';
+import { Home } from './pages/home/hotel-home';
 
 export const routes: Routes = [
-  { path: '', component: HotelUsuariosComponent }, // Página principals
+  { path: '', component: Home },
+  { path: 'cliente', component: HotelUsuariosComponent },
   { path: 'contactame', component: PaginaContactameComponent },
   { path: 'hotelhabitacion', component: HotelHabitaciones},
   { path: 'hotelpagos', component: HotelPagos},
-  { path: 'hotelreservas', component: HotelReservas}
+  { path: 'hotelreservas', component: HotelReservas},
+  { path: 'hotelusuarios', component: HotelUsuariosComponent },
 ];
